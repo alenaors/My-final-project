62d9a8a918d49e05b522d0b218d9fb7490360a18
alenaors <alenaors@gmail.com>
2025-06-06 13:11:10 +0200

    my commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,9 @@
 const Footer = () => {
     return (
       <footer className="footer">
-        <p className="footerText">📞 (111) 222-33-44</p>
+         <p className="footerText">
+        📞 <a href="tel:+1112223344" className="footerLink">(111) 222-33-44</a>
+      </p>
         <p className="footerText">📍 42155, Rosen street, 31, Flowerville</p>
       </footer>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,60 @@
 import { Link } from "react-router-dom";
 import logo from "../photo/logo.png";
 import { useTranslation } from "react-i18next";
+import { useState, useEffect, useRef } from "react";
 
 
 const Navbar = () => {
   const { t, i18n } = useTranslation();
+  const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef(null);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   const toggleLang = ()=> {
     i18n.changeLanguage(i18n.language==='en'?'ru':'en')
   }
 
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (menuRef.current && !menuRef.current.contains(e.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen]);
+
   return (
-    <nav>
+    <nav className="navbar">
       <div className="logo-container">
-      <img src={logo}  alt="Logo" className="logo"/>
-      <p className="logoName">Little Panda</p>
+        <img src={logo} alt="Logo" className="logo" />
+        <p className="logoName">Little Panda</p>
+        
       </div>
-      <div>
-      <Link to="/">{t('navbar.home')}</Link>
-        <Link to="/preschool">{t('navbar.preschool')}</Link>
-        <Link to="/speech-therapy">{t('navbar.speechTherapy')}</Link>
-        <Link to="/dance">{t('navbar.dance')}</Link>
-        <Link to="/montessori">{t('navbar.montessori')}</Link>
-      <button onClick={toggleLang} className="btnLang">{i18n.language==='en'?'RU':'EN'}</button>
+   <div className="links-container">
+      <div ref={menuRef} className={`nav-links ${isOpen ? "open" : ""}`}>
+        <Link to="/" onClick={() => setIsOpen(false)}>{t("navbar.home")}</Link>
+        <Link to="/preschool" onClick={() => setIsOpen(false)}>{t("navbar.preschool")}</Link>
+        <Link to="/speech-therapy" onClick={() => setIsOpen(false)}>{t("navbar.speechTherapy")}</Link>
+        <Link to="/dance" onClick={() => setIsOpen(false)}>{t("navbar.dance")}</Link>
+        <Link to="/montessori" onClick={() => setIsOpen(false)}>{t("navbar.montessori")}</Link>
       </div>
+      <button className="burger" onClick={toggleMenu}>
+          ☰
+        </button>
+      <button onClick={toggleLang} className="btnLang">
+          {i18n.language === "en" ? "RU" : "EN"}
+        </button>
+    </div>
     </nav>
   );
 };
